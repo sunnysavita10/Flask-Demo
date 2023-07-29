e8b6b30153fe9d3346547d4e6398f2c7dd4e24ef
sunnysavita10 <engsunny3365@gmail.com>
2023-07-29 11:16:21 +0530

    code added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,request,render_template
+from flask import Flask,request
 
 
 obj=Flask(__name__)
@@ -15,14 +15,14 @@ def math_operator():
     number2=request.json["number2"]
     
     if operation=="add":
-        result=number1+number2
+        result=int(number1)+int(number2)
     elif operation=="multiply":
-        result=number1*number2
+        result=int(number1)*int(number2)
     elif operation=="division":
-        result=number1/number2
+        result=int(number1)/int(number2)
     else:
-        result=number1-number2
-    return result
+        result=int(number1)-int(number2)
+    return "the operation is {} and the result is {}".format(operation,result)
         
     
 
